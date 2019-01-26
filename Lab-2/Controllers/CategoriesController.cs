0db7c8652b03ca85a7f1b1324b8fadc9f72d718f
s2d130050131049 <alaynavagamia96@gmail.com>
2019-01-25 21:41:35 -0500

    Lab-2

---
+++ Lab-2/Controllers/CategoriesController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Lab_2.Controllers
+{
+    public class CategoriesController : Controller
+    {
+        // GET: Categories
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Details(string Categories)
+        {
+            ViewBag.Categories = "You have selected category: "+Categories;
+            return View();
+        }
+    }
+}
